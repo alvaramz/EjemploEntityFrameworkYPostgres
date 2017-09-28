bbf9d1bfcbe9c5e75f0af663b97604f8bfd05e2b
alvaramz <adrian.alvarado_r@ucr.ac.cr>
2017-09-28 11:36:24 -0600

    Se agrega la primera versión de un ejemplo básico.

---
+++ WebSite2/App_Code/Parametro.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de Parametro
+/// </summary>
+[Table("parametros", Schema = "public")]
+public class Parametro
+{
+    public Parametro()
+    { }
+
+
+    [Key]
+    [Column("nombre")]
+    public string Nombre { get; set; }
+
+    [Column("valor")]
+    public string Valor { get; set; }
+        
+
+    
+}
---
+++ WebSite2/App_Code/db_Entities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de db_Entities
+/// </summary>
+public partial class db_Entities : DbContext
+{
+    
+        public db_Entities() : base(nameOrConnectionString: "psql") { }
+
+        public DbSet<Parametro> Parametros { get; set; }
+
+}
